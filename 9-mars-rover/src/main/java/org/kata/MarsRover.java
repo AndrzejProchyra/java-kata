7017ff173278f148f88dc8af19dd96022942d9b1
Andrzej Prochyra <andrzej@prochyra.io>
2024-06-28 16:19:47 +0100

    feat: really does not move if given empty commands

--- 9-mars-rover/src/main/java/org/kata/MarsRover.java
+++ 9-mars-rover/src/main/java/org/kata/MarsRover.java
@@ -3,6 +3,6 @@ package org.kata;
 public class MarsRover {
 
     public String execute(String commands) {
-        return "1 2 N";
+        return commands.split("\n")[1];
     }
 }
--- 9-mars-rover/src/test/java/org/kata/MarsRoverUnitTests.java
+++ 9-mars-rover/src/test/java/org/kata/MarsRoverUnitTests.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MarsRoverUnitTests {
 
     @Test
-    void should_not_move_if_given_no_commands() {
+    void should_not_move_if_on_1_2_and_given_no_commands() {
         String commands = "5 5\n1 2 N\n";
         MarsRover rover = new MarsRover();
 
@@ -15,4 +15,15 @@ class MarsRoverUnitTests {
 
         assertThat(finalPosition).isEqualTo("1 2 N");
     }
+
+    @Test
+    void should_not_move_if_on_1_3_and_given_no_commands() {
+        String commands = "5 5\n1 3 N\n";
+        MarsRover rover = new MarsRover();
+
+        String finalPosition = rover.execute(commands);
+
+        assertThat(finalPosition).isEqualTo("1 3 N");
+    }
+
 }
