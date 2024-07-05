e177bdb257fe797b442a969d5914fbe94444d274
Andrzej Prochyra <andrzej@prochyra.io>
2024-07-05 16:15:31 +0100

    refactor: start using Direction

--- 9-mars-rover/src/main/java/org/kata/MarsRover.java
+++ 9-mars-rover/src/main/java/org/kata/MarsRover.java
@@ -4,12 +4,13 @@ public class MarsRover {
 
     public String execute(String instructions) {
         String startingPosition = instructions.split("\n")[1];
+        String startingDirection = startingPosition.substring((startingPosition.length() - 1));
         String commands = extractCommands(instructions);
         if (commands.startsWith("LL")) {
             return startingPosition.substring(0, 3) + " S";
         }
         if (commands.startsWith("L")) {
-            return startingPosition.substring(0, 3) + " W";
+            return startingPosition.substring(0, 3) + " " + Direction.valueOf(startingDirection).turn("L");
         }
         if (commands.startsWith("R")) {
             return startingPosition.substring(0, 3) + " E";
