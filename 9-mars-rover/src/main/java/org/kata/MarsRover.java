eacfed733e2d6809087a2acead248b93cc53d271
Andrzej Prochyra <andrzej@prochyra.io>
2024-07-05 15:27:28 +0100

    refactor: extract method

--- 9-mars-rover/src/main/java/org/kata/MarsRover.java
+++ 9-mars-rover/src/main/java/org/kata/MarsRover.java
@@ -4,7 +4,7 @@ public class MarsRover {
 
     public String execute(String instructions) {
         String startingPosition = instructions.split("\n")[1];
-        String commands = instructions.split("\n").length > 2 ? instructions.split("\n")[2] : "";
+        String commands = extractCommands(instructions);
         if (commands.startsWith("LL")) {
             return startingPosition.substring(0, 3) + " S";
         }
@@ -15,5 +15,9 @@ public class MarsRover {
             return startingPosition.substring(0, 3) + " E";
         }
         return startingPosition;
+    }
+
+    private String extractCommands(String instructions) {
+        return instructions.split("\n").length > 2 ? instructions.split("\n")[2] : "";
     }
 }
