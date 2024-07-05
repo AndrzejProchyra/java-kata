4859344126c26e12df208840b7dd0d393d2f1c58
Andrzej Prochyra <andrzej@prochyra.io>
2024-07-05 16:05:14 +0100

    refactor: parameterize tests

--- 9-mars-rover/src/test/java/org/kata/CompassTest.java
+++ 9-mars-rover/src/test/java/org/kata/CompassTest.java
@@ -1,33 +1,21 @@
 package org.kata;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.kata.Direction.*;
 
 class DirectionTest {
 
-    @Test
-    void should_turn_left_from_North_to_West() {
-        assertThat(N.turn("L"))
-                .isEqualTo(W);
-    }
-
-    @Test
-    void should_turn_left_from_West_to_South() {
-        assertThat(W.turn("L"))
-                .isEqualTo(S);
-    }
-
-    @Test
-    void should_turn_left_from_South_to_East() {
-        assertThat(S.turn("L"))
-                .isEqualTo(E);
-    }
-
-    @Test
-    void should_turn_left_from_East_to_North() {
-        assertThat(E.turn("L"))
-                .isEqualTo(N);
+    @ParameterizedTest
+    @CsvSource({
+            "N, W",
+            "W, S",
+            "S, E",
+            "E, N"
+    })
+    void should_turn_left(Direction initialDirection, Direction finalDirection) {
+        assertThat(initialDirection.turn("L"))
+                .isEqualTo(finalDirection);
     }
 }
