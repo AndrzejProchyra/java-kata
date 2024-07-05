6201961c1b7aada2fb58fd633c42eff9e09a4d61
Andrzej Prochyra <andrzej@prochyra.io>
2024-07-05 15:16:55 +0100

    refactor: Extract assertion method

--- 9-mars-rover/src/test/java/org/kata/MarsRoverUnitTests.java
+++ 9-mars-rover/src/test/java/org/kata/MarsRoverUnitTests.java
@@ -8,32 +8,22 @@ class MarsRoverUnitTests {
 
     @Test
     void should_not_move_if_on_1_2_and_given_no_commands() {
-        String commands = "5 5\n1 2 N\n";
-        MarsRover rover = new MarsRover();
-
-        String finalPosition = rover.execute(commands);
-
-        assertThat(finalPosition).isEqualTo("1 2 N");
+        assertFinalPositionForInstructions("5 5\n1 2 N\n", "1 2 N");
     }
 
     @Test
     void should_not_move_if_on_1_3_and_given_no_commands() {
-        String commands = "5 5\n1 3 N\n";
-        MarsRover rover = new MarsRover();
-
-        String finalPosition = rover.execute(commands);
-
-        assertThat(finalPosition).isEqualTo("1 3 N");
+        assertFinalPositionForInstructions("5 5\n1 3 N\n", "1 3 N");
     }
 
     @Test
     void should_turn_left_starting_on_1_2_N() {
-        String commands = "5 5\n1 2 N\nL";
+        assertFinalPositionForInstructions("5 5\n1 2 N\nL", "1 2 W");
+    }
+
+    private void assertFinalPositionForInstructions(String instructions, String expectedFinalPosition) {
         MarsRover rover = new MarsRover();
-
-        String finalPosition = rover.execute(commands);
-
-        assertThat(finalPosition).isEqualTo("1 2 W");
-
+        String finalPosition = rover.execute(instructions);
+        assertThat(finalPosition).isEqualTo(expectedFinalPosition);
     }
 }
