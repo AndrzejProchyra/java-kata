4691cd0f9cfedc914191ac3dce226ce04822f80a
Andrzej Prochyra <andrzej@prochyra.io>
2024-07-05 16:08:38 +0100

    refactor: extract turning methods

--- 9-mars-rover/src/main/java/org/kata/Direction.java
+++ 9-mars-rover/src/main/java/org/kata/Direction.java
@@ -4,14 +4,19 @@ public enum Direction {
     N, W, S, E;
 
     public Direction turn(String command) {
-        if (command.equals("L")) {
-            return switch (this) {
-                case N -> W;
-                case S -> E;
-                case E -> N;
-                default -> S;
-            };
-        }
+        return command.equals("L") ? turnLeft() : turnRight();
+    }
+
+    private Direction turnLeft() {
+        return switch (this) {
+            case N -> W;
+            case S -> E;
+            case E -> N;
+            default -> S;
+        };
+    }
+
+    private Direction turnRight() {
         return switch (this) {
             case N -> E;
             case E -> S;
