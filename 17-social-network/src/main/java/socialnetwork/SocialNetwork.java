eb691696226a212ded7ffecc9280182b459aa05d
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-05 18:34:53 +0000

    add acceptance test

---
+++ 17-social-network/src/main/java/socialnetwork/SocialNetwork.java
@@ -0,0 +1,13 @@
+package socialnetwork;
+
+import java.util.List;
+
+public class SocialNetwork {
+    public List<String> timelineFor(String alice) {
+        return null;
+    }
+
+    public void post(String userName, String message) {
+
+    }
+}
---
+++ 17-social-network/src/test/java/socialnetwork/AcceptanceTest.java
@@ -0,0 +1,19 @@
+package socialnetwork;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class AcceptanceTest {
+
+    @Test
+    void given_Alice_posts_a_message_then_she_can_see_it_on_her_timeline() {
+        var socialNetwork = new SocialNetwork();
+        var message = "I love the weather today";
+
+        socialNetwork.post("Alice", message);
+
+        then(socialNetwork.timelineFor("Alice"))
+                .contains(message);
+    }
+}
