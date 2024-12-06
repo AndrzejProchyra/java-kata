467a0dbdd64e40c40096092df24f0620a484aaae
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-06 16:45:43 +0000

    refactor: mock interface, not implementation

--- 17-social-network/src/test/java/io/prochyra/socialnetwork/application/SocialNetworkTest.java
+++ 17-social-network/src/test/java/io/prochyra/socialnetwork/application/SocialNetworkTest.java
@@ -1,7 +1,7 @@
 package io.prochyra.socialnetwork.application;
 
 import io.prochyra.socialnetwork.application.model.User;
-import io.prochyra.socialnetwork.infra.persistence.InMemoryUserRepository;
+import io.prochyra.socialnetwork.application.model.UserRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.BDDMockito.then;
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.mock;
 class SocialNetworkTest {
     @Test
     void post_creates_a_user() {
-        var userRepository = mock(InMemoryUserRepository.class);
+        var userRepository = mock(UserRepository.class);
         var alice = new User("Alice");
         var socialNetwork = new SocialNetwork(userRepository);
 
