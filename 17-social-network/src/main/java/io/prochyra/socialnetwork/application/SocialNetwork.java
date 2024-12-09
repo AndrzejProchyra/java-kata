43849f1c900d18a100380540702e9774f03377e2
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-09 19:56:14 +0000

    test: take account of post timestamp in acceptance test

--- 17-social-network/src/main/java/io/prochyra/socialnetwork/application/SocialNetwork.java
+++ 17-social-network/src/main/java/io/prochyra/socialnetwork/application/SocialNetwork.java
@@ -3,16 +3,17 @@ package io.prochyra.socialnetwork.application;
 import io.prochyra.socialnetwork.application.model.User;
 import io.prochyra.socialnetwork.application.model.UserRepository;
 
+import java.time.InstantSource;
 import java.util.List;
 
 public class SocialNetwork {
     private final UserRepository userRepository;
 
-    public SocialNetwork(UserRepository userRepository) {
+    public SocialNetwork(UserRepository userRepository, InstantSource instantSource) {
         this.userRepository = userRepository;
     }
 
-    public List<String> timelineFor(String alice) {
+    public List<Post> timelineFor(String alice) {
         return null;
     }
 
