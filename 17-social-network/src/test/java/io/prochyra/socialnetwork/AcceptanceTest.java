d4ad60981971c6a5644ddb77341a56cd8d61618a
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-10 16:03:12 +0000

    test: add acceptance test for a user posting 2 messages and seeing them in reverse chronological order on their timeline

--- 17-social-network/src/test/java/io/prochyra/socialnetwork/AcceptanceTest.java
+++ 17-social-network/src/test/java/io/prochyra/socialnetwork/AcceptanceTest.java
@@ -1,10 +1,11 @@
 package io.prochyra.socialnetwork;
 
-import io.prochyra.socialnetwork.application.model.Post;
 import io.prochyra.socialnetwork.application.SocialNetwork;
+import io.prochyra.socialnetwork.application.model.Post;
 import io.prochyra.socialnetwork.infra.persistence.InMemoryUserRepository;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -23,5 +24,24 @@ class AcceptanceTest {
         var expectedPost = new Post(message, postTime);
         then(socialNetwork.timelineFor("Alice"))
                 .contains(expectedPost);
+    }
+
+    @Test
+    void given_Bob_posts_two_messages_he_can_see_them_on_his_timeline_in_reverse_chronological_order() {
+        var now = Instant.now();
+        var instantSource = new AdjustableInstantSource(now);
+        var socialNetwork = new SocialNetwork(new InMemoryUserRepository(), instantSource);
+        var firstMessage = "Damn! We lost!";
+        var secondMessage = "Good game though.";
+
+        socialNetwork.post("Bob", firstMessage);
+        instantSource.advanceBy(Duration.ofMinutes(1));
+        socialNetwork.post("Bob", secondMessage);
+
+        instantSource.advanceBy(Duration.ofMinutes(1));
+        var expectedFirstPost = new Post(firstMessage, now);
+        var expectedSecondPost = new Post(secondMessage, now.plus(Duration.ofMinutes(1)));
+        then(socialNetwork.timelineFor("Bob"))
+                .containsExactly(expectedFirstPost, expectedSecondPost);
     }
 }
