780892e66fcb6d43026dd3397922b91c305a7e62
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-10 13:07:48 +0000

    feat: user repository saves and finds by name

--- 17-social-network/src/main/java/io/prochyra/socialnetwork/application/model/UserRepository.java
+++ 17-social-network/src/main/java/io/prochyra/socialnetwork/application/model/UserRepository.java
@@ -1,5 +1,9 @@
 package io.prochyra.socialnetwork.application.model;
 
+import java.util.Optional;
+
 public interface UserRepository {
     void save(User user);
+
+    Optional<User> findByName(String username);
 }
