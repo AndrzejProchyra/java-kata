8938248eaf06de9aa7b34e67e22fec8fdd01f1ce
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-10 16:30:00 +0000

    refactor: clean up tests

--- 17-social-network/src/test/java/io/prochyra/socialnetwork/application/SocialNetworkTest.java
+++ 17-social-network/src/test/java/io/prochyra/socialnetwork/application/SocialNetworkTest.java
@@ -4,19 +4,27 @@ import io.prochyra.socialnetwork.AdjustableInstantSource;
 import io.prochyra.socialnetwork.application.model.Post;
 import io.prochyra.socialnetwork.application.model.User;
 import io.prochyra.socialnetwork.infra.persistence.InMemoryUserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
-import java.time.InstantSource;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 class SocialNetworkTest {
+
+    public static final Instant NOW = Instant.now();
+    private SocialNetwork socialNetwork;
+    private InMemoryUserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = new InMemoryUserRepository();
+        socialNetwork = new SocialNetwork(userRepository, new AdjustableInstantSource(NOW));
+    }
+
     @Test
     void post_creates_a_user_if_it_does_not_exist() {
-        var userRepository = new InMemoryUserRepository();
-        var socialNetwork = new SocialNetwork(userRepository, InstantSource.system());
-
         socialNetwork.post("Alice", "any message");
 
         then(userRepository.findByName("Alice"))
@@ -27,26 +35,19 @@ class SocialNetworkTest {
 
     @Test
     void post_publishes_a_message_to_a_user_timeline() {
-        var userRepository = new InMemoryUserRepository();
-        var now = Instant.now();
-        var socialNetwork = new SocialNetwork(userRepository, new AdjustableInstantSource(now));
-        String message = "The rain in Spain falls mainly on the plain.";
+        var message = "The rain in Spain falls mainly on the plain.";
 
         socialNetwork.post("Alice", message);
 
         var alice = userRepository.findByName("Alice");
         then(alice).isPresent();
         then(alice.get().timeLine())
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactly(new Post(message, now));
+                .containsExactly(new Post(message, NOW));
     }
 
     @Test
     void post_publishes_a_message_to_an_existing_user_timeline() {
-        var userRepository = new InMemoryUserRepository();
-        var now = Instant.now();
-        var socialNetwork = new SocialNetwork(userRepository, new AdjustableInstantSource(now));
-        var firstMessage = "The rain in Spain falls mainly on the plain.";
+        var firstMessage = "This is the first message";
         var secondMessage = "This is the second message";
 
         socialNetwork.post("Alice", firstMessage);
@@ -55,6 +56,6 @@ class SocialNetworkTest {
         var retrievedAlice = userRepository.findByName("Alice");
         then(retrievedAlice).isPresent();
         then(retrievedAlice.get().timeLine())
-                .containsExactly(new Post(firstMessage, now), new Post(secondMessage, now));
+                .containsExactly(new Post(firstMessage, NOW), new Post(secondMessage, NOW));
     }
 }
