b4f2339c5555577e69c24f18694a6d817af7b94c
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-11 19:34:58 +0000

    feat: user can follow another user

--- 17-social-network/src/main/java/io/prochyra/socialnetwork/application/model/User.java
+++ 17-social-network/src/main/java/io/prochyra/socialnetwork/application/model/User.java
@@ -10,10 +10,12 @@ public class User {
 
     private final String name;
     private final List<Post> timeline;
+    private final List<String> following;
 
     public User(String name) {
         this.name = name;
         timeline = new ArrayList<>();
+        following = new ArrayList<>();
     }
 
     public List<Post> timeLine() {
@@ -26,5 +28,13 @@ public class User {
 
     public void publish(String message, Instant timestamp) {
         timeline.add(new Post(message, timestamp));
+    }
+
+    public List<String> followedUsers() {
+        return List.copyOf(following);
+    }
+
+    public void follow(User otherUser) {
+        following.add(otherUser.name());
     }
 }
