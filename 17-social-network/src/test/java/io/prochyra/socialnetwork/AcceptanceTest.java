393b3df21515efa49ecf27d896cbb17f857c3f83
Andrzej Prochyra <andrzej@prochyra.io>
2024-12-11 16:34:50 +0000

    refactor: simplify test setup

--- 17-social-network/src/test/java/io/prochyra/socialnetwork/AcceptanceTest.java
+++ 17-social-network/src/test/java/io/prochyra/socialnetwork/AcceptanceTest.java
@@ -3,6 +3,7 @@ package io.prochyra.socialnetwork;
 import io.prochyra.socialnetwork.application.SocialNetwork;
 import io.prochyra.socialnetwork.application.model.Post;
 import io.prochyra.socialnetwork.infra.persistence.InMemoryUserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -12,36 +13,43 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 class AcceptanceTest {
 
+    SocialNetwork socialNetwork;
+    static final Instant NOW = Instant.now();
+    AdjustableInstantSource instantSource;
+
+    @BeforeEach
+    void setUp() {
+        instantSource = new AdjustableInstantSource(NOW);
+        socialNetwork = new SocialNetwork(new InMemoryUserRepository(), instantSource);
+    }
+
     @Test
     void given_Alice_posts_a_message_then_she_can_see_it_on_her_timeline() {
-        var postTime = Instant.now();
-        var instantSource = new AdjustableInstantSource(postTime);
-        var socialNetwork = new SocialNetwork(new InMemoryUserRepository(), instantSource);
         var message = "I love the weather today";
 
         socialNetwork.post("Alice", message);
 
-        var expectedPost = new Post(message, postTime);
+        var expectedPost = new Post(message, NOW);
         then(socialNetwork.timelineFor("Alice"))
                 .contains(expectedPost);
     }
 
     @Test
     void given_Bob_posts_two_messages_he_can_see_them_on_his_timeline_in_reverse_chronological_order() {
-        var now = Instant.now();
-        var instantSource = new AdjustableInstantSource(now);
-        var socialNetwork = new SocialNetwork(new InMemoryUserRepository(), instantSource);
         var firstMessage = "Damn! We lost!";
         var secondMessage = "Good game though.";
 
         socialNetwork.post("Bob", firstMessage);
-        instantSource.advanceBy(Duration.ofMinutes(1));
+        advanceTimeByMinutes(1);
         socialNetwork.post("Bob", secondMessage);
 
-        instantSource.advanceBy(Duration.ofMinutes(1));
-        var expectedFirstPost = new Post(firstMessage, now);
-        var expectedSecondPost = new Post(secondMessage, now.plus(Duration.ofMinutes(1)));
+        var expectedFirstPost = new Post(firstMessage, NOW);
+        var expectedSecondPost = new Post(secondMessage, NOW.plus(Duration.ofMinutes(1)));
         then(socialNetwork.timelineFor("Bob"))
-                .containsExactly(expectedFirstPost, expectedSecondPost);
+                .containsExactly(expectedSecondPost, expectedFirstPost);
+    }
+
+    private void advanceTimeByMinutes(int minutes) {
+        instantSource.advanceBy(Duration.ofMinutes(minutes));
     }
 }
