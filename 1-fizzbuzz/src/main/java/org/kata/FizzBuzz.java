4023f7939a27a09259865274f52d720fe19b4598
Andrzej Prochyra <andrzej@prochyra.io>
2025-04-26 21:28:41 +0100

    FizzBuzz of 3 is "Fizz"

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.kata;
 
 public class FizzBuzz {
     String fizzBuzzOf(int n) {
+        if (n == 3) {
+            return "fizz";
+        }
         return Integer.toString(n);
     }
 }
