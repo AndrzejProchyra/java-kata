180f959bb97ebec794fb1e73a8ce90f775ac87f4
shafqat <shafqat0074@gmail.com>
2025-04-26 21:03:25 +0100

    tests passing for numbers

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,9 +2,6 @@ package org.kata;
 
 public class FizzBuzz {
     String fizzBuzzOf(int n) {
-        if (n == 1) {
-            return "1";
-        }
-        return "2";
+        return Integer.toString(n);
     }
 }
