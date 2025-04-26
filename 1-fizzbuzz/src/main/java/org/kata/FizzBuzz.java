ea1e00ed28a3408a54b400b8e04728041d63ff35
shafqat <shafqat0074@gmail.com>
2025-04-26 21:37:34 +0100

    number divisible of 3 is fizz

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.kata;
 
 public class FizzBuzz {
     String fizzBuzzOf(int n) {
-        if (n == 3) {
+        if (n%3 == 0) {
             return "fizz";
         }
         return Integer.toString(n);
